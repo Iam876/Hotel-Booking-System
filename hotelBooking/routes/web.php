67abf6598fa1e8ff1cfe5e5033d2029551c93a95
Iam876 <imuhammad784@gmail.com>
2024-02-26 19:05:23 +0600

    Logout Functionality Implemented. Also the guidance has provided into the Project-Note.txt

--- hotelBooking/routes/web.php
+++ hotelBooking/routes/web.php
@@ -24,4 +24,5 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'roleMiddleware:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
